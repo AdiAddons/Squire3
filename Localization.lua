d1e9deaf34bb231d22cf4e3d73544aa882abb1ca
Adirelle <adirelle@gmail.com>
2014-10-27 23:25:03 +0100

    Translations updated.

--- Localization.lua
+++ Localization.lua
@@ -70,8 +70,10 @@ L["Select a modifier to select a ground mount even in flyable area."] = true
 L["Select which action Squire3 should automatically take."] = true
 L["Select which spells Squire3 should use."] = true
 L["Shift"] = true
+L["Two-step mode"] = true
 L["Unsafe modifier"] = true
 L["Use spells"] = true
+L["When enabled, Squire3 will either dismount or mount each click, not both at the same time."] = true
 L["When enabled, automatically dismount when you cast a spell mid-air. When disabled, you have to dismount first."] = true
 L["When enabled, automatically dismount when you cast a spell. When disabled, you have to dismount first."] = true
 L["When enabled, trying to cast a spell normally unavailable to your current shapeshift form automatically cancels it beforehands. When disabled, you have to unshift first."] = true
@@ -112,16 +114,83 @@ L["When enabled, automatically dismount when you cast a spell. When disabled, yo
 L["When enabled, trying to cast a spell normally unavailable to your current shapeshift form automatically cancels it beforehands. When disabled, you have to unshift first."] = "Quand actif, lancer un sort normalement interdit pour la transformation courante l'annule. Quand inactif, il faut d'abord annuler la transformation." -- Needs review
 
 ------------------------ deDE ------------------------
--- no translation
+elseif locale == 'deDE' then
+L["Alt"] = "ALT"
+L["Any"] = "Jeder"
+L["Automatically ..."] = "Automatisch..."
+L["Blizzard settings"] = "Blizzard-Einstellungen"
+L["Cancel %s"] = "%s abbrechen"
+L["Control"] = "STRG"
+L["Dismount"] = "Absitzen"
+L["Dismount while flying"] = "Abzitzen im Flug"
+L["Leave vehicle"] = "Fahrzeug verlassen"
+L["Macro"] = "Makro"
+L["None"] = "Keiner"
+L["Options"] = "Einstellungen"
+L["Shift"] = "SHIFT"
 
 ------------------------ esMX ------------------------
--- no translation
+elseif locale == 'esMX' then
+L["Alt"] = "Alt"
+L["Any"] = "Cualquier"
+L["Automatically ..."] = "Automáticamente..."
+L["Blizzard settings"] = "Opciones de Blizzard"
+L["Built-in settings that interacts with shapeshift forms and mounts."] = "Opciones que se incluyen en el juego y se afectan el comportamiento de formas y monturas."
+L["Cancel %s"] = "Cancelar %s"
+L["Control"] = "Ctrl"
+L["Create and pickup a macro to put in an action slot."] = "Crea y recoge un macro para poner en un botón de acción."
+L["Dismount"] = "Desmontar"
+L["Dismount while flying"] = "Desmontar en vuelo"
+L["Enforce ground mount modifier"] = "Modificador para usar montura de tierra"
+L["Leave vehicle"] = "Salir del vehículo"
+L["Macro"] = "Macro"
+L["None"] = "Ningún"
+L["Note: these are console variables. They are not affected by profile changes."] = "Notas: Estos son variables de la consola. Ellas no se ven afectadas por los cambios del perfíl."
+L["Options"] = "Opciones"
+L["Select a modifier to enforce a unsafe behavior (like dismount mid-air)."] = "Seleccione un modificador para ejecutar un acción inseguro, como desmontar en vuelo."
+L["Select a modifier to select a ground mount even in flyable area."] = "Seleccione un modificador para usar una montura de tierra en una zona donde puedes volar."
+L["Select which action Squire3 should automatically take."] = "Seleccione el acción que Squire3 debe hacer automáticamente."
+L["Select which spells Squire3 should use."] = "Seleccione los hechizos que Squire3 debe usar."
+L["Shift"] = "Mayús"
+L["Unsafe modifier"] = "Modificador inseguro"
+L["Use spells"] = "Usar hechizos"
+L["Use Squire3"] = "Usar Squire3"
+L["When enabled, automatically dismount when you cast a spell mid-air. When disabled, you have to dismount first."] = "Cuando se activa, desmontas automáticamente al lanzar un hechizo en vuelo. Cuando no se activa, primero debes desmontar."
+L["When enabled, automatically dismount when you cast a spell. When disabled, you have to dismount first."] = "Cuando se activa, desmontas automáticamente al lanzar un hechizo. Cuando no se activa, primero debes desmontar."
+L["When enabled, trying to cast a spell normally unavailable to your current shapeshift form automatically cancels it beforehands. When disabled, you have to unshift first."] = "Cuando se activa, cancelas automáticamente el cambio de forma al lanzar un hechizo que no puede ser lanzado en tu forma actuál. Cuando no se activa, primero debes cancelar la forma."
 
 ------------------------ ruRU ------------------------
 -- no translation
 
 ------------------------ esES ------------------------
--- no translation
+elseif locale == 'esES' then
+L["Alt"] = "Alt"
+L["Any"] = "Cualquier"
+L["Automatically ..."] = "Automáticamente..."
+L["Blizzard settings"] = "Opciones de Blizzard"
+L["Built-in settings that interacts with shapeshift forms and mounts."] = "Opciones que se incluyen en el juego y se afectan el comportamiento de formas y monturas."
+L["Cancel %s"] = "Cancelar %s"
+L["Control"] = "Ctrl"
+L["Create and pickup a macro to put in an action slot."] = "Crea y recoge un macro para poner en un botón de acción."
+L["Dismount"] = "Desmontar"
+L["Dismount while flying"] = "Desmontar en vuelo"
+L["Enforce ground mount modifier"] = "Modificador para usar montura de tierra"
+L["Leave vehicle"] = "Salir del vehículo"
+L["Macro"] = "Macro"
+L["None"] = "Ningún"
+L["Note: these are console variables. They are not affected by profile changes."] = "Notas: Estos son variables de la consola. Ellas no se ven afectadas por los cambios del perfíl."
+L["Options"] = "Opciones"
+L["Select a modifier to enforce a unsafe behavior (like dismount mid-air)."] = "Seleccione un modificador para ejecutar un acción inseguro, como desmontar en vuelo."
+L["Select a modifier to select a ground mount even in flyable area."] = "Seleccione un modificador para usar una montura de tierra en una zona donde puedes volar."
+L["Select which action Squire3 should automatically take."] = "Seleccione el acción que Squire3 debe hacer automáticamente."
+L["Select which spells Squire3 should use."] = "Seleccione los hechizos que Squire3 debe usar."
+L["Shift"] = "Mayús"
+L["Unsafe modifier"] = "Modificador inseguro"
+L["Use spells"] = "Usar hechizos"
+L["Use Squire3"] = "Usar Squire3"
+L["When enabled, automatically dismount when you cast a spell mid-air. When disabled, you have to dismount first."] = "Cuando se activa, desmontas automáticamente al lanzar un hechizo en vuelo. Cuando no se activa, primero debes desmontar."
+L["When enabled, automatically dismount when you cast a spell. When disabled, you have to dismount first."] = "Cuando se activa, desmontas automáticamente al lanzar un hechizo. Cuando no se activa, primero debes desmontar."
+L["When enabled, trying to cast a spell normally unavailable to your current shapeshift form automatically cancels it beforehands. When disabled, you have to unshift first."] = "Cuando se activa, cancelas automáticamente el cambio de forma al lanzar un hechizo que no puede ser lanzado en tu forma actuál. Cuando no se activa, primero debes cancelar la forma."
 
 ------------------------ zhTW ------------------------
 -- no translation
@@ -130,7 +199,34 @@ L["When enabled, trying to cast a spell normally unavailable to your current sha
 -- no translation
 
 ------------------------ koKR ------------------------
--- no translation
+elseif locale == 'koKR' then
+L["Alt"] = "Alt"
+L["Any"] = "Any"
+L["Automatically ..."] = "자동화"
+L["Blizzard settings"] = "블리자드 설정"
+L["Built-in settings that interacts with shapeshift forms and mounts."] = "태세 변환과 탈것에 대한 행동을 변경하는 설정입니다."
+L["Cancel %s"] = "취소 %s"
+L["Control"] = "Ctrl"
+L["Create and pickup a macro to put in an action slot."] = "매크로를 생성하여 행동 바에 넣습니다."
+L["Dismount"] = "해제"
+L["Dismount while flying"] = "비행시 해제"
+L["Enforce ground mount modifier"] = "지상 탈것 강제 기능키"
+L["Leave vehicle"] = "탈것 내리기"
+L["Macro"] = "매크로"
+L["None"] = "None"
+L["Note: these are console variables. They are not affected by profile changes."] = "중요: 이것은 콘솔의 변수입니다. 프로필 변경이 적용되지 않습니다."
+L["Options"] = "설정"
+L["Select a modifier to enforce a unsafe behavior (like dismount mid-air)."] = "불완전한 행동을 할 수 있는 기능키를 선택합니다. (이를테면 하늘에서 내리기)"
+L["Select a modifier to select a ground mount even in flyable area."] = "비행가능 지역에서 지상 탈것으로 선택할 때 기능키를 선택합니다."
+L["Select which action Squire3 should automatically take."] = "비행하는 동안 Squire3가 자동으로 진행해야 하는 것을 선택합니다."
+L["Select which spells Squire3 should use."] = "Squire3가 사용하는 주문을 선택합니다."
+L["Shift"] = "Shift"
+L["Unsafe modifier"] = "불완전한 행동 기능키"
+L["Use spells"] = "주문 사용"
+L["Use Squire3"] = "Squire3 사용"
+L["When enabled, automatically dismount when you cast a spell mid-air. When disabled, you have to dismount first."] = "선택하면 하늘에서 주문을 시전하면 자동으로 탈것에서 내립니다. 탈것에서 먼저 내리면 불가능합니다."
+L["When enabled, automatically dismount when you cast a spell. When disabled, you have to dismount first."] = "선택하면 주문을 시전할 때 탈것에서 내립니다. 탈것에서 먼저 내리면 불가능합니다."
+L["When enabled, trying to cast a spell normally unavailable to your current shapeshift form automatically cancels it beforehands. When disabled, you have to unshift first."] = "선택하면 태세 또는 변신시 사용할 수 없는 주문을 시전할 때 자동으로 먼저 취소합니다. 반드시 변신전에 시전해야 합니다."
 
 ------------------------ ptBR ------------------------
 -- no translation
